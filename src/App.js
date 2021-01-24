11a50b98fe77e49409aa0a86ce22b8fd4d4ec3d5
Henrique <henriquefons@hotmail.com>
2021-01-24 14:55:23 -0300

    ajuda ae

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import axios from "axios";
 
- const URI = "http://18.230.65.107:5000/";
- const user1 = {
-   "company"  : 1,
-   "username" : "admin",
-   "senha" : "admin"
- };
-
- const authAxios = axios.create({
-   baseURL: URI,
-   headers: {
-     "sessionToken": "a55b4f502d2d3b45bbe6e1f74a7b838c-1-1-a55b4f502d2d3b45bbe6e1f74a7b838c",
-     "Content-Type": "application/json",
-     "Access-Control-Allow-Origin": "*",
-     "Access-Control-Allow-Headers": "Content-Type",
-   },
- });
-
 const App = () => {
-   const [tokenAcess, setTokenAcess] = useState([]);
-   const [requestError, setRequestError] = useState();
-
-   const fetchData = useCallback(async () => {
-     try {
-       const result = await authAxios.get(`${URI}/auth/user`, { data: user1 });
-       setTokenAcess(result.data);
-       console.log(tokenAcess);
-     } catch (err) {
-       setRequestError(err.message);
-       console.log(requestError);
-     }
-   });
+  async function requestUrl() {
+    try {
+      await axios({
+        method: "get",
+        url: "http://18.230.65.107:5000/auth/user",
+        data: {
+          company: 1,
+          username: "admin",
+          senha: "admin",
+        },
+      }).then(function (response) {
+        if (response.status < 400) {
+          console.log(response.data);
+        }
+      });
+    } catch (error) {
+      console.log("error");
+    }
+  }
 
   return (
     <div className="App">
-      <button onClick={() => fetchData()}>Chamando a API </button>
+      <button onClick={() => requestUrl()}>Chamando a API </button>
     </div>
   );
 };
+
+export default App;
 
 export default App;
